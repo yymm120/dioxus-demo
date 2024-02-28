b7848d8afffd0f5218921dfe193c59ef561dc4bb
root <root@xinhao>
2024-02-29 02:20:46 +0800

    desktop

---
+++ src/server.rs
@@ -0,0 +1,25 @@
+// Run with:
+// ```bash
+// cargo run --bin server --features server
+// ```
+
+use axum_desktop::*;
+use dioxus_fullstack::prelude::*;
+// use server_fn::axum::register_explicit;
+
+#[tokio::main]
+async fn main() {
+  let addr = std::net::SocketAddr::from(([127, 0, 0, 1], 8080));
+
+    let _ = PostServerData::register_explicit();
+    let _ = GetServerData::register_explicit();
+
+    axum::Server::bind(&addr)
+        .serve(
+            axum::Router::new()
+                .register_server_fns("")
+                .into_make_service(),
+        )
+        .await
+        .unwrap();
+}
